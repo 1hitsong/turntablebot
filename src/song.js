bbba57e2e43ee4ce742518413c8ed7f9ea11dac7
1hitsong <3330318+1hitsong@users.noreply.github.com>
2021-06-10 21:36:29 -0400

    Add method for bot to snag song

--- src/song.js
+++ src/song.js
@@ -1,13 +1,21 @@
+const request = require('request');
 const Vote = require('./vote');
+const Room = require('./room');
+
 
 module.exports.onNewSong = async (bot, data) => {
-    console.log(`New Song`);
-    
     // Up vote every song
     sleep(5000).then(() => {
-        console.log(`Bop`);
         Vote(bot, `up`);
     });
 }
+
+module.exports.snag = async (bot) => {
+    const roomInfo = await Room.getInfo(bot);
+    let currentSongId = roomInfo.room.metadata.current_song._id;
+    
+    bot.playlistAdd(currentSongId);
+    bot.snag();
+};
 
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
