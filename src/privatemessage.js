f511be866188a2f4e085c8d13b8a7d71092af62b
1hitsong <3330318+1hitsong@users.noreply.github.com>
2021-06-10 21:37:38 -0400

    Add methods to command bot to start & stop djing and to snag song

--- src/privatemessage.js
+++ src/privatemessage.js
@@ -1,4 +1,5 @@
 const Vote = require('./vote');
+const Song = require('./song');
 
 module.exports.onNewPM = async (bot, data) => {
 
@@ -7,6 +8,10 @@ module.exports.onNewPM = async (bot, data) => {
         `
         [ /ban * ]
         [ /lyrics artist - song title ]
+        [ /otherbands ]
+        [ /released ]
+        [ /spotify-artist ]
+        [ /spotify-song ]
         [ /what genre is * ]
         [ /wiki * ]
         [ /yt * ]
@@ -15,6 +20,24 @@ module.exports.onNewPM = async (bot, data) => {
 
     if (data.text.match(/^\/bop/)) {
         Vote(bot, `up`);
+    }
+
+    if (data.text.match(/^\/snag/)) {
+        if (data.senderid === '60986c3e47c69b001ad5c5f5') {
+            Song.snag(bot);
+        }
+    }
+
+    if (data.text.match(/^\/djstart/)) {
+        if (data.senderid === '60986c3e47c69b001ad5c5f5') {
+            bot.addDj();
+        }
+    }
+
+    if (data.text.match(/^\/djstop/)) {
+        if (data.senderid === '60986c3e47c69b001ad5c5f5') {
+            bot.remDj();
+        }
     }
 };
 
